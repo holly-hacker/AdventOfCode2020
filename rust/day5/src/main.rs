8f35c9dbade64f3501a40f696d5655a18dc07fb5
HoLLy <dario.db@hotmail.com>
2020-12-05 19:19:03 +0100

    Day 5: move code to functions

--- rust/day5/src/main.rs
+++ rust/day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::{convert::TryInto, io::Read, time::Duration, time::Instant};
+use std::convert::TryInto;
 
 // NOTE: could be made cleaner without overloads RFC2000 gets implementerd
 // https://rust-lang.github.io/rfcs/2000-const-generics.html
@@ -59,45 +59,51 @@ fn solve_part_2(data: &[bool; KEYSPACE]) -> usize {
         .0
 }
 
-fn time<T, F>(fun: F) -> (T, Duration)
+fn time<T, F>(fun: F) -> (T, std::time::Duration)
 where
     F: FnOnce() -> T,
 {
-    let now = Instant::now();
+    let now = std::time::Instant::now();
     let ret = fun();
     let elapsed = now.elapsed();
     (ret, elapsed)
 }
 
+fn read_stdin() -> String {
+    use std::io::Read;
+    let mut string = String::new();
+    std::io::stdin().lock().read_to_string(&mut string).unwrap();
+    string
+}
+
 const KEYSPACE: usize = 2 << (10 - 1);
 fn main() {
-    let (input_data, time_read) = time(|| {
-        let mut string = String::new();
-        std::io::stdin().lock().read_to_string(&mut string).unwrap();
-        let input = string
-            .lines()
-            .map(|line| line.as_bytes().try_into().unwrap())
-            .collect::<Vec<[u8; 10]>>();
-        input
-    });
-
-    let (data, time_parse) = time(|| {
-        let mut data = [false; KEYSPACE];
-        for f in input_data {
-            let decoded = decode_partition_10(&f);
-            data[decoded] = true;
-        }
-        data
-    });
+    let (input_data, time_read) = time(|| split(read_stdin()));
+    let (data, time_parse) = time(|| parse_input(input_data));
     let (solution_1, time_1) = time(|| solve_part_1(&data));
     let (solution_2, time_2) = time(|| solve_part_2(&data));
-    
+
     println!("solution 1: {}", solution_1);
     println!("solution 2: {}", solution_2);
-    println!("Time to read input to string: {:?}", time_read);
+    println!("Time to read input: {:?}", time_read);
     println!("Time to parse input: {:?}", time_parse);
     println!("Time to solve 1: {:?}", time_1);
     println!("Time to solve 2: {:?}", time_2);
+}
+
+fn split(str: String) -> Vec<[u8; 10]> {
+    str.lines()
+        .map(|line| line.as_bytes().try_into().unwrap())
+        .collect::<Vec<[u8; 10]>>()
+}
+
+fn parse_input(input_data: Vec<[u8; 10]>) -> [bool; KEYSPACE] {
+    let mut data = [false; KEYSPACE];
+    for f in input_data {
+        let decoded = decode_partition_10(&f);
+        data[decoded] = true;
+    }
+    data
 }
 
 #[cfg(test)]
@@ -122,4 +128,7 @@ mod tests {
         assert_eq!(119, decode_partition_10(b"FFFBBBFRRR"));
         assert_eq!(820, decode_partition_10(b"BBFFBBFRLL"));
     }
+
+    #[test]
+    fn test_solution_1() {}
 }
