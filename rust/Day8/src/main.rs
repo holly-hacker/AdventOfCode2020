324c45171ebddb0d64c33e4a149015cbc46951d0
HoLLy <dario.db@hotmail.com>
2020-12-08 10:33:33 +0100

    Day 8: Solve part 1

--- rust/Day8/src/main.rs
+++ rust/Day8/src/main.rs
@@ -1,15 +1,54 @@
+use std::todo;
+
 include!("../../helpers.rs");
 
-struct VirtualMachine {
+#[derive(Default)]
+struct VirtualMachine<'a> {
     accumulator: i32,
-    instructions: Vec<Instruction>,
+    instructions: &'a [Instruction],
     instruction_pointer: usize,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+impl<'a> VirtualMachine<'a> {
+    pub fn new(instructions: &'a [Instruction]) -> Self {
+        Self {
+            instructions,
+            ..Default::default()
+        }
+    }
+
+    pub fn run_instruction(&mut self) {
+        let instruction = self.instructions[self.instruction_pointer];
+        let mut instruction_jump = None;
+        match instruction {
+            Instruction::Acc(i) => {
+                self.accumulator += i as i32;
+            }
+            Instruction::Jmp(i) => {
+                instruction_jump = Some(i as i32);
+            }
+            Instruction::Nop => (),
+        }
+
+        self.instruction_pointer = (self.instruction_pointer as i32
+            + match instruction_jump {
+                Some(j) => j,
+                None => 1,
+            }) as usize;
+    }
+
+    pub fn get_ip(&self) -> usize {
+        self.instruction_pointer
+    }
+    pub fn get_acc(&self) -> i32 {
+        self.accumulator
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Instruction {
-    Acc(i8),
-    Jmp(i8),
+    Acc(i16),
+    Jmp(i16),
     Nop,
 }
 
@@ -28,6 +67,23 @@ impl Instruction {
 fn main() {
     let (stdin, time_reading) = time(|| read_stdin());
     let (input, time_parsing) = time(|| parse_input(&stdin));
+    let (solution_1, time_solving_1) = time(|| solve_1(&input));
+
+    println!("solution 1: {}", solution_1);
+}
+
+fn solve_1(instructions: &Vec<Instruction>) -> i32 {
+    let mut recursion_stack = vec![]; // TODO: can be optimized with stack array
+    let mut vm = VirtualMachine::new(instructions);
+
+    loop {
+        let ip = vm.get_ip();
+        if recursion_stack.contains(&ip) {
+            return vm.get_acc();
+        }
+        recursion_stack.push(ip);
+        vm.run_instruction();
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Instruction> {
@@ -54,7 +110,7 @@ mod tests {
         acc +6";
 
     #[test]
-    fn test() {
+    fn test_parsing() {
         let parsed = parse_input(INPUT);
 
         let expected = vec![
@@ -73,5 +129,11 @@ mod tests {
         for i in 0..parsed.len() {
             assert_eq!(expected[i], parsed[i]);
         }
+    }
+
+    #[test]
+    fn test_solution_1() {
+        let parsed = parse_input(INPUT);
+        assert_eq!(5, solve_1(&parsed));
     }
 }
