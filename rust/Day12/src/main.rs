2c6e7a6609746f56950904e3b093010412e55f4e
HoLLy <dario.db@hotmail.com>
2020-12-12 17:59:27 +0100

    Day 12: Solve part 2

--- rust/Day12/src/main.rs
+++ rust/Day12/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use challenge::*;
 
 include!("../../helpers.rs");
@@ -5,29 +7,44 @@ include!("../../helpers.rs");
 fn main() {
     let (stdin, time_reading) = time(|| read_stdin());
     let (input, time_parsing) = time(|| Instruction::parse(&stdin));
-    let (solution_1, time_solving_1) = time(|| {
-        ShipPosition::default()
-            .run_instructions(&input)
-            .location
-            .get_manhattan_distance()
-    });
+    let (solution_1, time_solving_1) = time(|| run_for::<ShipPosition>(&input));
+    let (solution_2, time_solving_2) = time(|| run_for::<ShipWithWaypoint>(&input));
 
     println!("solution 1: {}", solution_1);
-    // println!("solution 2: {}", solution_2);
+    println!("solution 2: {}", solution_2);
     println!("took {:?} to read stdin", time_reading);
     println!("took {:?} to read input", time_parsing);
     println!("took {:?} to solve 1", time_solving_1);
-    // println!("took {:?} to solve 2", time_solving_2);
+    println!("took {:?} to solve 2", time_solving_2);
+}
+
+fn run_for<T>(instructions: &[Instruction]) -> usize
+where
+    T: Default,
+    T: Debug,
+    T: CanSolveChallenge,
+{
+    T::default()
+        .run_instructions(instructions)
+        .get_manhattan_distance()
 }
 
 mod challenge {
+    use std::fmt::Debug;
+
+    #[derive(Debug)]
+    pub struct ShipWithWaypoint {
+        pub ship_location: Location,
+        pub waypoint_offset: Location,
+    }
+
     #[derive(Default, Debug)]
     pub struct ShipPosition {
         pub location: Location,
         pub direction: Direction,
     }
 
-    #[derive(Default, Debug)]
+    #[derive(Default, Debug, Copy, Clone)]
     pub struct Location(pub i32, pub i32);
 
     pub struct Instruction {
@@ -53,16 +70,74 @@ mod challenge {
         Forward,
     }
 
-    impl ShipPosition {
-        pub fn run_instructions(self, instructions: &[Instruction]) -> Self {
-            let mut x = self;
+    pub trait CanSolveChallenge {
+        fn run_instructions(mut self, instructions: &[Instruction]) -> Self
+        where
+            Self: Sized,
+            Self: Debug,
+        {
             for instruction in instructions {
-                x = x.run_instruction(instruction);
+                self = self.run_instruction(instruction);
             }
 
-            x
+            self
         }
 
+        fn run_instruction(self, instruction: &Instruction) -> Self;
+        fn get_manhattan_distance(&self) -> usize;
+    }
+
+    impl Default for ShipWithWaypoint {
+        fn default() -> Self {
+            Self {
+                ship_location: Location::default(),
+                waypoint_offset: Location(10, 1),
+            }
+        }
+    }
+
+    impl CanSolveChallenge for ShipWithWaypoint {
+        fn run_instruction(self, instruction: &Instruction) -> Self {
+            match instruction.action {
+                Action::North => Self {
+                    waypoint_offset: self.waypoint_offset.with_y_offset(instruction.value),
+                    ..self
+                },
+                Action::South => Self {
+                    waypoint_offset: self.waypoint_offset.with_y_offset(-instruction.value),
+                    ..self
+                },
+                Action::East => Self {
+                    waypoint_offset: self.waypoint_offset.with_x_offset(instruction.value),
+                    ..self
+                },
+                Action::West => Self {
+                    waypoint_offset: self.waypoint_offset.with_x_offset(-instruction.value),
+                    ..self
+                },
+                Action::Left => Self {
+                    waypoint_offset: self.waypoint_offset.rotate(-instruction.value),
+                    ..self
+                },
+                Action::Right => Self {
+                    waypoint_offset: self.waypoint_offset.rotate(instruction.value),
+                    ..self
+                },
+                Action::Forward => Self {
+                    ship_location: self
+                        .ship_location
+                        .move_in_direction(self.waypoint_offset, instruction.value),
+                    ..self
+                },
+            }
+        }
+
+        fn get_manhattan_distance(&self) -> usize {
+            self.ship_location.get_manhattan_distance()
+        }
+    }
+
+    impl CanSolveChallenge for ShipPosition {
         fn run_instruction(self, instruction: &Instruction) -> Self {
             match instruction.action {
                 Action::North => Self {
@@ -97,6 +172,10 @@ mod challenge {
                 },
             }
         }
+
+        fn get_manhattan_distance(&self) -> usize {
+            self.location.get_manhattan_distance()
+        }
     }
 
     impl Location {
@@ -121,6 +200,22 @@ mod challenge {
             };
 
             Self(self.0 + amount * x, self.1 + amount * y)
+        }
+
+        pub fn move_in_direction(self, direction: Location, amount: i32) -> Self {
+            let (x, y) = (direction.0, direction.1);
+            Self(self.0 + amount * x, self.1 + amount * y)
+        }
+
+        pub fn rotate(self, degrees: i32) -> Self {
+            debug_assert!(degrees.abs() % 90 == 0);
+            match (degrees + 360) % 360 {
+                90 => Self {
+                    0: self.1,
+                    1: -self.0,
+                },
+                _ => self.rotate(90).rotate(degrees - 90),
+            }
         }
     }
 
@@ -181,14 +276,24 @@ mod tests {
     const TEST_INPUT: &str = "F10\nN3\nF7\nR90\nF11";
 
     #[test]
-    fn test() {
+    fn test_1() {
         let parsed = Instruction::parse(TEST_INPUT);
         let position = ShipPosition::default();
         let position = position.run_instructions(&parsed);
-        let distance = position.location.get_manhattan_distance();
 
         assert_eq!(17, position.location.0);
         assert_eq!(-8, position.location.1);
-        assert_eq!(25, distance);
+        assert_eq!(25, position.get_manhattan_distance());
+    }
+
+    #[test]
+    fn test_2() {
+        let parsed = Instruction::parse(TEST_INPUT);
+        let position = ShipWithWaypoint::default();
+        let position = position.run_instructions(&parsed);
+
+        assert_eq!(214, position.ship_location.0);
+        assert_eq!(-72, position.ship_location.1);
+        assert_eq!(286, position.get_manhattan_distance());
     }
 }
