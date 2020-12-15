b2608fed0bcdd6bc52ddda0dde8a608be3816005
HoLLy <dario.db@hotmail.com>
2020-12-15 10:10:58 +0100

    Day 15: Solve part 2, albeit slowly

--- rust/Day15/src/main.rs
+++ rust/Day15/src/main.rs
@@ -5,31 +5,31 @@ include!("../../helpers.rs");
 fn main() {
     let (stdin, time_reading) = time(|| read_stdin());
     let (input, time_parsing) = time(|| parse(&stdin));
-    let (solution_1, time_solving_1) = time(|| solve_1(&input));
-    // let (solution_2, time_solving_2) = time(|| solve_2(&input));
+    let (solution_1, time_solving_1) = time(|| solve(&input, 2020));
+    let (solution_2, time_solving_2) = time(|| solve(&input, 30000000));
 
     println!("solution 1: {}", solution_1);
-    // println!("solution 2: {}", solution_2);
+    println!("solution 2: {}", solution_2);
     println!("took {:?} to read stdin", time_reading);
     println!("took {:?} to read input", time_parsing);
     println!("took {:?} to solve 1", time_solving_1);
-    // println!("took {:?} to solve 2", time_solving_2);
+    println!("took {:?} to solve 2", time_solving_2);
 }
 
 fn parse(s: &str) -> Vec<usize> {
     s.split(',').map(|x| x.parse().unwrap()).collect()
 }
 
-fn solve_1(data: &[usize]) -> usize {
+fn solve(data: &[usize], limit: usize) -> usize {
     let mut map = HashMap::<usize, usize>::new();
     for (i, &val) in data.iter().enumerate().take(data.len() - 1) {
-        println!("Adding starting number {}", val);
+        // println!("Adding starting number {}", val);
         map.insert(val, i); // 0-0, 3-1, 6-2
     }
 
     let mut next = *data.iter().last().unwrap();
     // println!("Adding starting number {}", next);
-    for i in (data.len() - 1)..2019 {
+    for i in (data.len() - 1)..(limit-1) {
         let next2 = map
             .get(&next)
             .map(|&n| {
@@ -53,17 +53,33 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let solved = solve_1(&[0, 3, 6]);
+        let solved = solve(&[0, 3, 6], 2020);
         assert_eq!(436, solved);
     }
 
     #[test]
     fn test_1_extra() {
-        assert_eq!(1, solve_1(&[1, 3, 2]));
-        assert_eq!(10, solve_1(&[2, 1, 3]));
-        assert_eq!(27, solve_1(&[1, 2, 3]));
-        assert_eq!(78, solve_1(&[2, 3, 1]));
-        assert_eq!(438, solve_1(&[3, 2, 1]));
-        assert_eq!(1836, solve_1(&[3, 1, 2]));
+        assert_eq!(1, solve(&[1, 3, 2], 2020));
+        assert_eq!(10, solve(&[2, 1, 3], 2020));
+        assert_eq!(27, solve(&[1, 2, 3], 2020));
+        assert_eq!(78, solve(&[2, 3, 1], 2020));
+        assert_eq!(438, solve(&[3, 2, 1], 2020));
+        assert_eq!(1836, solve(&[3, 1, 2], 2020));
+    }
+
+    #[test]
+    fn test_2() {
+        let solved = solve(&[0, 3, 6], 30000000);
+        assert_eq!(175594, solved);
+    }
+
+    #[test]
+    fn test_2_extra() {
+        assert_eq!(2578, solve(&[1, 3, 2], 30000000));
+        assert_eq!(3544142, solve(&[2, 1, 3], 30000000));
+        assert_eq!(261214, solve(&[1, 2, 3], 30000000));
+        assert_eq!(6895259, solve(&[2, 3, 1], 30000000));
+        assert_eq!(18, solve(&[3, 2, 1], 30000000));
+        assert_eq!(362, solve(&[3, 1, 2], 30000000));
     }
 }
