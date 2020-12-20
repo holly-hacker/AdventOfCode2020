39743ce33c93f9ded636d371a3825fbe69ca8475
HoLLy <dario.db@hotmail.com>
2020-12-20 19:56:08 +0100

    Day 19: Solve part 2

--- rust/Day19/src/main.rs
+++ rust/Day19/src/main.rs
@@ -6,17 +6,20 @@ fn main() {
     let (stdin, time_reading) = time(|| read_stdin());
     let (input, time_parsing) = time(|| Input::parse(&stdin));
     let (solution_1, time_solving_1) = time(|| input.solve_1());
-    // let (solution_2, time_solving_2) = time(|| solve(&input, 30000000));
+    let (solution_2, time_solving_2) = time(|| input.solve_2());
 
     println!("solution 1: {}", solution_1);
-    // println!("solution 2: {}", solution_2);
+    println!("solution 2: {}", solution_2);
     println!("took {:?} to read stdin", time_reading);
     println!("took {:?} to read input", time_parsing);
     println!("took {:?} to solve 1", time_solving_1);
-    // println!("took {:?} to solve 2", time_solving_2);
+    println!("took {:?} to solve 2", time_solving_2);
+
+    assert!(solution_2 > 226);
 }
 
 mod challenge {
+    #[derive(Clone)]
     pub struct Input<'a> {
         rules: Vec<Rule>,
         messages: Vec<&'a str>,
@@ -54,6 +57,13 @@ mod challenge {
                 .filter(|&&m| self.rules[0].check(m, &self.rules))
                 .count()
         }
+
+        pub fn solve_2(&self) -> usize {
+            let mut copy = self.clone();
+            copy.rules[8] = Rule::Or(vec![42], vec![42, 8]);
+            copy.rules[11] = Rule::Or(vec![42, 31], vec![42, 11, 31]);
+            copy.solve_1()
+        }
     }
 
     impl Rule {
@@ -84,47 +94,53 @@ mod challenge {
 
         fn check(&self, input: &str, all_rules: &[Rule]) -> bool {
             let checked = self.check_inner(input, all_rules);
-            checked.0 && checked.1.len() == 0
+            checked.iter().any(|&x| x.is_empty())
         }
 
-        fn check_inner<'a>(&self, input: &'a str, all_rules: &[Rule]) -> (bool, &'a str) {
+        fn check_inner<'a>(&self, input: &'a str, all_rules: &[Rule]) -> Vec<&'a str> {
             match self {
-                Rule::Constant(c) => (input.bytes().nth(0) == Some(*c), &input[1..]),
-                Rule::And(list) => Self::check_multiple(&list, input, all_rules),
-                Rule::Or(list1, list2) => {
-                    let (b1, s1) = Self::check_multiple(&list1, input, all_rules);
-                    let (b2, s2) = Self::check_multiple(&list2, input, all_rules);
-
-                    // not supporting different length patterns
-                    if b1 && b2 {
-                        debug_assert_eq!(s1.len(), s2.len());
-                        debug_assert_eq!(s1, s2);
-                    }
-
-                    if b1 {
-                        (true, s1)
-                    } else if b2 {
-                        (true, s2)
+                Rule::Constant(c) => {
+                    if input.bytes().nth(0) == Some(*c) {
+                        vec![&input[1..]]
                     } else {
-                        (false, "")
+                        vec![]
                     }
+                }
+                Rule::And(rules) => Self::check_all_rules_match(&rules, input, all_rules).into(),
+                Rule::Or(rules1, rules2) => {
+                    let paths1 = Self::check_all_rules_match(&rules1, input, all_rules);
+                    let paths2 = Self::check_all_rules_match(&rules2, input, all_rules);
+
+                    let mut x = paths1.clone();
+                    x.extend(paths2);
+                    x
                 }
             }
         }
 
-        fn check_multiple<'a>(
+        fn check_all_rules_match<'a>(
             rules: &[usize],
-            mut input: &'a str,
+            input: &'a str,
             all_rules: &[Rule],
-        ) -> (bool, &'a str) {
-            for &idx in rules {
-                let (b, input2) = all_rules[idx].check_inner(input, all_rules);
-                input = input2;
-                if !b {
-                    return (false, "");
-                }
+        ) -> Vec<&'a str> {
+            if rules.is_empty() {
+                return vec![input];
             }
-            (true, input)
+
+            let rule = *rules.first().unwrap();
+            let rule = &all_rules[rule];
+            let rule_paths = rule.check_inner(input, all_rules);
+
+            if rule_paths.is_empty() {
+                return vec![];
+            }
+
+            // check next part
+            let remaining_rules = &rules[1..];
+            rule_paths
+                .iter()
+                .flat_map(|&path| Self::check_all_rules_match(remaining_rules, path, all_rules))
+                .collect()
         }
     }
 
@@ -153,9 +169,23 @@ mod tests {
         aaabbb\n\
         aaaabbb";
 
+    const TEST_INPUT_2: &str = include_str!("../test_input.txt");
+
     #[test]
     fn test_1() {
         let parsed = Input::parse(TEST_INPUT);
         assert_eq!(2, parsed.solve_1());
+    }
+
+    #[test]
+    fn test_2_1() {
+        let parsed = Input::parse(TEST_INPUT_2);
+        assert_eq!(3, parsed.solve_1());
+    }
+
+    #[test]
+    fn test_2_2() {
+        let parsed = Input::parse(TEST_INPUT_2);
+        assert_eq!(12, parsed.solve_2());
     }
 }
